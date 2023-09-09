feb51f975d53157418bbb839eb3d77787a9c3e4c
aridel96 <adelgado.162@gmail.com>
2023-09-09 02:37:49 -0400

    Javascript: Added html using js. Got data for current weather from openweather

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -4,12 +4,9 @@ var longitude
 
 var geoKey
 
-// var search1
-// var search2
-// var search3
-// var count = 0
-
-var recentSearch = []
+var recentSearch = {}           // Object holding all recent searches for localstorage
+var history
+var searchResults 
 
 var cityEl = document.getElementById("city"); 
 var countryEl = document.getElementById("country")
@@ -18,6 +15,7 @@ var city
 var country
 
 var limit = 5
+var count = 0               // counter to keep track of recent searches in recentSearch array
 
 var unitedStates = "US"
 var usCodes = [                                 // List of US State codes for the openWeather's GeoCoding API
@@ -27,11 +25,14 @@ var usCodes = [                                 // List of US State codes for th
     "VI", "WA", "WV", "WI"
 ]
 
-var title = document.querySelector('h1');
 var button = document.querySelector('button');
+var imageDiv = document.getElementById("image");
+var divEl = document.getElementById("inputEl");
+var header = document.getElementById('heading');
+var icon;
 
 function getWeather(city) {
-    city = cityEl.value;
+    city = cityEl.value;                    
     country = countryEl.value
     // console.log(city + ", " + country)
 
@@ -51,40 +52,146 @@ function getWeather(city) {
         .then(function(data) {
             latitude = data[0].lat; 
             longitude = data[0].lon;
+ 
+            recentSearch[count] = city + ", " + country             // Sets count as the key and the city and state as the value for recentSearch object
 
-            var weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${weatherKey}`;       // OpenWeather API URL
-            var search = city + ", " + country;
+            localStorage.setItem("search", JSON.stringify(recentSearch))        // Turn object to string to save in localstorage
 
-            recentSearch.push(search);
+            currentWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherKey}&units=imperial`
+            
 
-            localStorage.setItem("searches", JSON.stringify(recentSearch))
+            fetch(currentWeatherAPI)
+                .then(function(currentData) {
+                    imageDiv.innerHTML = " ";               // Set elements innerHTML to blank to reset page
+                    divEl.innerHTML = " ";
+                    header.innerHTML = " ";
 
-            document.location.href = "./results.html"
+                    return currentData.json();
+                })
 
-            fetch(weatherAPI)
-                .then(function(response) {
-                    console.log(response)
-                    var history = localStorage.getItem("Searches")
-                    JSON.parse(history)
+                .then(function(currentWeather) {
+                    // console.log(currentWeather);
 
-                    console.log(history)
-                    title.textContent = history[history.length - 1];
+                    divEl.classList = [];
+                    divEl.classList.add("row", "pb-5");
+
+                    header.innerHTML = `                
+                        <h1></h1>
+                        <a href="./index.html">
+                            <i class="fa-solid fa-house"> Try Again?</i>
+                        </a>`
+
+                    var title = document.querySelector('h1');
+                    title.textContent = currentWeather.name + ", " + country;
+
+                    divEl.innerHTML =  `
+                        <div class="card currentDate" style="width: 18rem;">
+                            <div class="card-body text-center">
+                                <h2 id="crntDate"></h2>                   <!--Hook for current date-->
+                                <img id="crntIcon" alt="weather icon"></img>                     <!--Hook for current weather icon-->
+                                <p class="card-text" id="crntTemp">Temperature: </p>
+                                <p class="card-text" id="crntHumidity">Humidity: </p>
+                                <p id="crntWind">Wind Speeds: </p>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body text-center">
+                                <h2 id="day1"></h2>
+                                <img id="d1Icon" alt="weather icon"></img>
+                                <p class="card-text" id="d1Temp">Temperature: </p>
+                                <p class="card-text" id="d1Humidity">Humidity: </p>
+                                <p id="d1Wind">Wind Speeds: </p>
+                            </div>
+                        </div>    
+
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body text-center">
+                                <h2 id="day2"></h2>
+                                <img id="d2Icon" alt="weather icon"></img>
+                                <p class="card-text" id="d2Temp">Temperature: </p>
+                                <p class="card-text" id="d2Humidity">Humidity: </p>
+                                <p id="d2Wind">Wind Speeds: </p>
+                            </div>
+                        </div>
+                    
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body text-center">
+                                <h2 id="day3"></h2>
+                                <img id="d3Icon" alt="weather icon"></img>
+                                <p class="card-text" id="d3Temp">Temperature: </p>
+                                <p class="card-text" id="d3Humidity">Humidity: </p>
+                                <p id="d3Wind">Wind Speeds: </p>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body text-center">
+                                <h2 id="day4"></h2>
+                                <img id="d4Icon" alt="weather icon"></img>
+                                <p class="card-text" id="d4Temp">Temperature: </p>
+                                <p class="card-text" id="d4Humidity">Humidity: </p>
+                                <p id="d4Wind">Wind Speeds: </p>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body text-center">
+                                <h2 id="day5"></h2>
+                                <img id="d5Icon" alt="weather icon"></img>
+                                <p class="card-text" id="d5Temp">Temperature: </p>
+                                <p class="card-text" id="d5Humidity">Humidity: </p>
+                                <p id="d5Wind">Wind Speeds: </p>
+                            </div>
+                        </div>`
+
+                    var crntDate = document.getElementById('crntDate');
+                    // crntDate.textContent =  CALL DAYJS API
+
+                    var crntIcon = document.getElementById('crntIcon');
+                    icon = currentWeather.weather[0].icon;
+                    crntIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`
+
+                    var crntTemp = document.getElementById('crntTemp');
+                    crntTemp.textContent = "Temperature: " + currentWeather.main.temp;
+
+                    var crntHumidity = document.getElementById('crntHumidity');
+                    crntHumidity.textContent = "Humidity: " + currentWeather.main.humidity;
+
+                    var crntWind = document.getElementById('crntWind');
+                    crntWind.textContent = "Wind Speeds: " + currentWeather.wind.speed
+
+                    var weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${weatherKey}&units=imperial`;       // OpenWeather API URL
                 
-            })
+                    fetch(weatherAPI)
+                        .then(function(weatherData) {
+                            history = JSON.parse(localStorage.getItem("search"))        // Get item from local storage and convert to object
+
+                            console.log(weatherData.json())
+
+                            // searchResults = history[0]
 
 
+                            // document.location.href = "./results.html"
 
+                            return weatherData.json();                
+                        })
 
+                        .then(function(info) {
 
-        
+                            // console.log(info);
+
+                        })
+                })
     })
 
+    count++                         // increment count by 1
 }
 
 
 // console.log(window.location.href.split('/'))
 
-if (window.location.href.split('/')[8] == 'index.html') {                   // Once deployed change index
+if (window.location.href.split('/')[window.location.href.split('/').length - 1] == 'index.html') {                   // Once deployed change index
     button.addEventListener('click', function() {
         getWeather(city)
     })
